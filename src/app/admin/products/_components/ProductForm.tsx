3aa81706c2fc7fd128ff83cbd5a48a51779ef447
Iago Boardy <iagogol2006@gmail.com>
2024-08-27 23:28:20 -0300

    Products config form

--- src/app/admin/products/_components/ProductForm.tsx
+++ src/app/admin/products/_components/ProductForm.tsx
@@ -7,12 +7,14 @@ import { Textarea } from "@/components/ui/textarea"
 import { formatCurrency } from "@/lib/formatters"
 import { useState } from "react"
 import { addProduct } from "../../_actions/product"
+import { useFormState, useFormStatus } from "react-dom" //CONTINUAR NESSA PAGINA NO VIDEO E EM PRODUCT TS
 
 export function ProductForm() {
+  const [error, action] = useFormState(addProduct, {})
   const [priceInCents, setPriceInCents] = useState<number>() 
 
   return <>
-  <form action={addProduct} className="space-y-8">
+  <form action={action} className="space-y-8">
     <div className="space-y-2">
       <Label htmlFor="name">Name</Label>
       <Input type="text" id="name" name="name" required/>
@@ -38,8 +40,18 @@ export function ProductForm() {
       <Label htmlFor="image">Image</Label>
       <Input type="file" id="image" name="image" required/>
     </div>
-    <Button type="submit">Save</Button>
+    <SubmitButton/>
   </form>
   
   </>
+}
+
+function SubmitButton() {
+  const { pending } = useFormStatus();
+  
+  return (
+    <Button type="submit" disabled={pending}>
+      {pending ? "Saving..." : "Save"}
+    </Button>
+  );
 }
